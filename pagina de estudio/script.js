2bd9e14dedae4e58dadd67f14a467a9ae4afd78a
shidow346 <alexandercardosso@gmail.com>
2024-06-08 18:20:29 -0600

    Add files via upload

---
+++ pagina de estudio/script.js
@@ -0,0 +1,85 @@
+function respuesta(){
+    swal('FELICIDADES','respuesta correcta','success')
+}
+
+function respuesta2(){
+    swal('HAS FALLADO','respuesta incorrecta','error')
+ }
+ function respuesta3(){
+    swal('HAS FALLADO','respuesta incorrecta','error')
+}
+
+function respuesta4(){
+    swal('FELICIDADES','respuesta correcta','success')
+}
+
+function respuesta5(){
+    swal('HAS FALLADO','respuesta incorrecta','error')
+ }
+    
+function respuesta6(){
+    swal('FELICIDADES','respuesta correcta','success')
+}
+function respuesta7(){
+    swal('FELICIDADES','respuesta correcta','success')
+ }
+    
+function respuesta8(){
+swal('HAS FALLADO','respuesta incorrecta','error')
+ }
+  function respuesta9(){
+    swal('FELICIDADES','respuesta correcta','success')
+}
+        
+ function respuesta10(){
+ swal('HAS FALLADO','respuesta incorrecta','error')
+}
+ function respuesta11(){
+    swal('FELICIDADES','respuesta correcta','success')
+}
+        
+ function respuesta12(){
+ swal('HAS FALLADO','respuesta incorrecta','error')
+ }
+        
+ function respuesta13(){
+    swal('HAS FALLADO','respuesta incorrecta','error')
+}
+            
+ function respuesta14(){
+    swal('FELICIDADES','respuesta correcta','success')
+ }
+ function respuesta15(){
+    swal('HAS FALLADO','respuesta incorrecta','error')
+ }
+ function respuesta16(){
+    swal('FELICIDADES','respuesta correcta','success')
+  }
+  function respuesta17(){
+    swal('HAS FALLADO','respuesta incorrecta','error')
+   }
+           
+    function respuesta18(){
+        swal('FELICIDADES','respuesta correcta','success')
+   }
+    function respuesta19(){
+        swal('FELICIDADES','respuesta correcta','success')
+   }
+           
+    function respuesta20(){
+    swal('HAS FALLADO','respuesta incorrecta','error')
+    }
+           
+    function respuesta21(){
+        swal('FELICIDADES','respuesta correcta','success')
+   }
+               
+    function respuesta22(){
+    swal('HAS FALLADO','respuesta incorrecta','error')
+    }
+    function respuesta23(){
+        swal('HAS FALLADO','respuesta incorrecta','error')
+    }
+    function respuesta24(){
+        swal('FELICIDADES','respuesta correcta','success')
+     }
